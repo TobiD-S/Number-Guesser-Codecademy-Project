62eca93846cbee57bcc1b31f2ee18d8591b22d04
Tobi <delesafatobi@gmail.com>
2020-04-14 23:35:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+var generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
 
+console.log(generateTarget());
